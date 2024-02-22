94179e3542c952aed883add8aefe5c387285a889
henrikszucs <henrikszucs6@gmail.com>
2024-02-22 13:48:39 +0100

    update + new version

--- 03 Calendar/index.js
+++ 03 Calendar/index.js
@@ -22,16 +22,18 @@ const addEvent = function(dayIndex, startTimeStr, endTimeStr, label) {
     startVal -= 7 * 60;
     endVal -= 7 * 60;
 
-    startVal = startVal / 60 / 13 * 100;
-    endVal = 100 - endVal / 60 / 13 * 100;
+    startVal = startVal / 60 * 100;
+    const startValOffset = (startVal/100 - 1) * 1;
+    endVal = 100 - endVal / 60 * 100;
+    const endValOffset = (endVal / 100 - 1) * 1;
 
     const newSpan = document.createElement("span");
     newSpan.classList.add("event");
-    newSpan.style.top = startVal + "%";
-    newSpan.style.bottom = endVal + "%";
+    newSpan.style.top = "calc(" + startVal + "%" + " + " + startValOffset + "px)";
+    newSpan.style.bottom = "calc(" + endVal + "%" + " + " + endValOffset + "px)";
     newSpan.innerText = labelVal;
 
-    const el = document.querySelector(".schedule table:nth-child(2) tbody tr td:nth-child(" + (dayVal + 2) + ") div");
+    const el = document.querySelector(".schedule table tbody tr:nth-child(1) td:nth-child(" + (dayVal + 2) + ") div");
     el.appendChild(newSpan);
 }
 
@@ -67,6 +69,13 @@ window.addEventListener("load", function () {
 
     addEvent(3, "14:50", "16:25", "High performance computing");
     addEvent(3, "16:30", "18:05", "High performance computing");
+
+    for (let i = 7; i < 20; i++) {
+        addEvent(4, i.toString().padStart(2, 0)+":00", (i+1).toString().padStart(2, 0)+":00", "Test");
+        
+    }
+
+    
 
     addEl.addEventListener("click", function() {
         addEvent(dayEl.selectedIndex, startEl.value, endEl.value, labelEl.value);
